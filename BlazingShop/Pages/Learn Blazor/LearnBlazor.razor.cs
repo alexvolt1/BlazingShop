fb761b6ad463ed49c759ac891e664a36b16c3145
alexV <alexV@DESKTOP-UKJJB1E>
2019-12-08 02:02:08 -0500

    Add project files.

---
+++ BlazingShop/Pages/Learn Blazor/LearnBlazor.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingShop.Pages.Learn_Blazor
+{
+    public class LearnBlazorBase:ComponentBase
+    {
+        protected string name = "Alex";
+        protected string welcomeText = "Time to learn Blazor!";
+        protected void getName()
+        {
+            name = "Blazor Learner";
+        }
+    }
+}
