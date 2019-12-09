ada425d3a1ee9dd720a5e9dd14dbc1381d60375d
alexV <alexV@DESKTOP-UKJJB1E>
2019-12-08 23:17:03 -0500

    comm

--- BlazingShop/Data/ApplicationDbContext.cs
+++ BlazingShop/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BlazingShop.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,6 @@ namespace BlazingShop.Data
             : base(options)
         {
         }
+        public DbSet<Category> Categories { get; set; }
     }
 }
